4163565451a381fa2b461d39280094024b2717c6
Zana Cranmer <acranmer@bentley.edu>
2025-08-08 14:56:11 -0400

    Remove package

--- ghgi_app.py
+++ ghgi_app.py
@@ -11,7 +11,7 @@ import pandas as pd
 import plotly.express as px
 #import plotly.graph_objects as go
 #from plotly.subplots import make_subplots
-import streamlit_analytics as streamlit_analytics
+#import streamlit_analytics as streamlit_analytics
 
 
 start_year = 2017
